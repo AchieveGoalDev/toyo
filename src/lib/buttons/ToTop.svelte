f8ad9d50f940276b4186c929d183e0d5289937a1
Justin <thejasumasa@gmail.com>
2023-01-24 12:31:48 +0900

    add to top button

---
+++ src/lib/buttons/ToTop.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  //@ts-ignore
+  import * as animateScroll from "svelte-scrollto";
+  import { scrollHeight } from "$lib/store/windowData";
+  import { fly } from "svelte/transition";
+
+  let scrolled: number;
+  scrollHeight.subscribe((v) => (scrolled = v));
+</script>
+
+{#if scrolled > 100}
+  <div class="relative">
+    <button
+      transition:fly={{ duration: 200, y: -30 }}
+      on:click={() => animateScroll.scrollToTop({ duration: 500 })}
+      class="shadow-lg fixed bottom-[50px] right-[50px] rounded-full text-white text-lg lg:text-xl font-bold bg-rose-600 p-4 lg:p-5"
+    >
+      ページTOPへ
+    </button>
+  </div>
+{/if}
--- src/routes/(site)/faq/+page.svelte
+++ src/routes/(site)/faq/+page.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-	import HighlightTitle from '$lib/highlightTitle.svelte';
-	import { collections } from '$lib/faq/FaqDefs';
-	import Collection from '$lib/faq/Collection.svelte';
-	import LinkList from '$lib/faq/LinkList.svelte';
-	import Question from '$lib/faq/Question.svelte';
+  import HighlightTitle from "$lib/highlightTitle.svelte";
+  import { collections } from "$lib/faq/FaqDefs";
+  import Collection from "$lib/faq/Collection.svelte";
+  import LinkList from "$lib/faq/LinkList.svelte";
+  import Question from "$lib/faq/Question.svelte";
+  import ToTop from "$lib/buttons/ToTop.svelte";
 </script>
 
 <div class="w-[full] px-5 pt-10">
-    <HighlightTitle pretext="よくある" highlight="質問" posttext="" />
-	<LinkList {collections}/>
-	{#each collections as collection}
-		<h1 id={'id' + collection.id}>{collection.title}</h1>
-		{#each collection.questions as question}
-			<Question {question} />
-		{/each}
-	{/each}
+  <HighlightTitle pretext="よくある" highlight="質問" posttext="" />
+  <LinkList {collections} />
+  {#each collections as collection}
+    <h1 id={"id" + collection.id}>{collection.title}</h1>
+    {#each collection.questions as question}
+      <Question {question} />
+    {/each}
+  {/each}
 </div>
+<ToTop />
