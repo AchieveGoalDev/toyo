b8f427cbb3935b84fd9299bca69e13247735ab9f
Justin <thejasumasa@gmail.com>
2023-02-17 15:34:27 +0900

    refactor component and scroll functionality

--- src/lib/testimonials/Quote.svelte
+++ src/lib/testimonials/Quote.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
+  import { fly,slide } from "svelte/transition";
   import type { TestimonialProfile } from "$lib/testimonials/TestimonialData";
 
   export let data: TestimonialProfile;
+  export let expanded: boolean
+
+  $: console.log(data);
 </script>
 
+
+{#if !expanded}
 <div
+  transition:slide
   class="flex flex-row items-center justify-items-center place-content-around py-2 px-[50px]"
 >
   <figure class="flex flex-col items-center">
@@ -13,10 +20,38 @@
       alt={data.imgAlt}
       class="h-[200px] p-2 bg-yellow-50 rounded-full shadow-md"
     />
-    <figcaption class="text-2xl"><strong>{data.name}</strong>さん</figcaption>
-    <strong class="text-2xl text-rose-900">{"TOEICスコア" + data.scoreUp + "点アップ!"}</strong>
+    <figcaption class="text-2xl">
+      <strong>{data.name}</strong>さん
+    </figcaption>
+    <strong class="text-2xl text-rose-900"
+      >{"TOEICスコア" + data.scoreUp + "点アップ!"}</strong
+    >
   </figure>
   <blockquote class="text-5xl text-rose-800 font-bold max-w-[75%]">
     {data.quote}
   </blockquote>
 </div>
+{:else }
+<div
+  transition:fly
+  class="flex flex-row items-center justify-items-center place-content-around py-2 px-[50px]"
+>
+  <figure class="flex flex-col items-center">
+    <img
+      src={data.img}
+      alt={data.imgAlt}
+      class="h-[200px] p-2 bg-yellow-50 rounded-full shadow-md"
+    />
+    <figcaption class="text-2xl">
+      <strong>{data.name}</strong>さん
+    </figcaption>
+    <strong class="text-2xl text-rose-900"
+      >{"TOEICスコア" + data.scoreUp + "点アップ!"}</strong
+    >
+  </figure>
+  <blockquote class="text-5xl text-rose-800 font-bold max-w-[75%]">
+    {data.quote}
+  </blockquote>
+</div>
+{/if}
+
--- src/lib/testimonials/Testimonial.svelte
+++ src/lib/testimonials/Testimonial.svelte
@@ -3,33 +3,48 @@
   import { slide, fade } from "svelte/transition";
   import Quote from "$lib/testimonials/Quote.svelte";
   import Profile from "$lib/testimonials/Profile.svelte";
-  import { data } from "./TestimonialData";
-
+  import { data, type TestimonialProfile } from "./TestimonialData";
 
   let s3 = PUBLIC_IMAGE_BUCKET_URL;
   let index = 0;
   let expanded = false;
+  let display = true;
+  let currentData = data[0];
 
   function handleLeft() {
-    let max = data.length - 1
-    if (index === 0){
-      index = max
+    display = false;
+    let max = data.length - 1;
+    if (index === 0) {
+      index = max;
     } else {
-      index--
+      index--;
+    }
+    display = true;
+  }
+
+  function handleRight() {
+    display = false;
+    let max = data.length - 1;
+    if (index === max) {
+      index = 0;
+    } else {
+      index++;
     }
   }
 
-  function handleRight() { 
-    let max = data.length - 1
-    if (index === max){
-      index = 0
-    } else {
-      index++
+  const cycler = (direction: string) => {
+    display = false;
+
+    if (direction === "left") {
+      handleLeft();
+    } else if (direction === "right") {
+      handleRight();
     }
-  }
 
-  $: console.log(index)
+    currentData = data[index];
 
+    setTimeout(() => (display = true), 400);
+  };
 </script>
 
 <div class="w-full shadow-lg bg-slate-10 relative min-h-[250px]">
@@ -37,33 +52,42 @@
     {#if !expanded}
       <button
         transition:fade
-        on:click={handleLeft}
+        on:click={() => cycler("left")}
         class="absolute left-0 top-1/3 text-xl rounded-tr-full rounded-br-full bg-rose-800 text-white text-xl py-2 pl-1 transition-all opacity-50 hover:bg-rose-700 hover:opacity-100 h-[100px]"
-        >
-        <img
-        alt={"前へ"}
-        src={s3 + "images/chevron_left_white.svg"}
-        class="h-[30px]"
-      />
-        </button
       >
+        <img
+          alt={"前へ"}
+          src={s3 + "images/chevron_left_white.svg"}
+          class="h-[30px]"
+        />
+      </button>
       <button
-        on:click={handleRight} 
-        transition:fade 
+        disabled={!display}
+        on:click={() => cycler("right")}
+        transition:fade
         class="absolute right-0 top-1/3 text-xl rounded-tl-full rounded-bl-full bg-rose-800 text-white text-xl py-2 pl-1 transition-all opacity-50 hover:bg-rose-700 hover:opacity-100 h-[100px]"
-        >
-        <img
-        alt={"次へ"}
-        src={s3 + "images/chevron_right_white.svg"}
-        class="h-[30px]"
-      />
-        </button
       >
-      <div class="mx-auto text-center shadow-lg" transition:slide>
-        <Quote data={data[index]} />
-      </div>
+        <img
+          alt={"次へ"}
+          src={s3 + "images/chevron_right_white.svg"}
+          class="h-[30px]"
+        />
+      </button>
+      {#key index}
+        <div
+          style:display={"grid"}
+          style:grid-row={"1"}
+          style:grid-column={"1"}
+          class="mx-auto text-center shadow-lg"
+        >
+          {#if display && currentData}
+            <Quote data={currentData} bind:expanded />
+          {/if}
+        </div>
+      {/key}
     {/if}
     <button
+      disabled={!display}
       on:click={() => (expanded = !expanded)}
       class="font-bold text-2xl pb-2 w-full bg-rose-800 text-white transition-all ease-in hover:bg-rose-700 hover:scale-[102%]"
       >{expanded ? "閉じる" : "詳細を見る"}</button
