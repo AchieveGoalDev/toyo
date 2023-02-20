24bf974293052a30ee9759e827142323a2c21c49
Jasumasa <thejasumasa@gmail.com>
2023-02-20 23:16:42 +0900

    create validation functionality for text input component

--- src/lib/forms/GroupLesson.svelte
+++ src/lib/forms/GroupLesson.svelte
@@ -2,6 +2,39 @@
   import TextInput from "$lib/forms/TextInput.svelte";
 
   let 学籍番号: string;
+  let 学籍番号isValid: boolean;
+  const validate学籍番号 = (stNumber: string) => {
+    let errors: string[] = [];
+    if (stNumber) {
+      let stringArray = stNumber.split("");
+      let hasFull: boolean = false;
+
+      stringArray.forEach((letter) => {
+        if (letter.match(/[^\x01-\x7E]/) || letter.match(/[^\uFF65-\uFF9F]/)) {
+          hasFull = true;
+        }
+      });
+
+      if (hasFull) {
+        errors.push("半角文字で入力してください");
+      }
+
+      if (stringArray[0] && typeof parseInt(stringArray[0]) !== "number") {
+        errors.push("学籍番号はアルファベットから始まることはありません");
+      }
+
+      if (stringArray.length < 10 && stringArray.length > 0) {
+        errors.push("10文字以上にしてください");
+      }
+
+      if (stringArray.length === 0) {
+        errors.push("必要項目です");
+      }
+    }
+    return errors;
+  };
+
+  $: console.log(学籍番号);
 </script>
 
 <div class="flex flex-col">
@@ -22,11 +55,16 @@
   </fieldset>
 
   <TextInput
+    bind:inputVal={学籍番号}
+    bind:isValid={学籍番号isValid}
     label={"学籍番号"}
     isNecessary={true}
-    desc={"アルファベット入力の際は大文字で入力してください。学籍番号がアルファベットから始まることはありません。"}
-    bind:inputVal={学籍番号}
+    desc={[
+      "アルファベット入力の際は大文字で入力してください。",
+      "学籍番号がアルファベットから始まることはありません。",
+    ]}
     alt={"学籍番号の入力"}
-    placeholder=""
+    placeholder="test"
+    validateInput={validate学籍番号}
   />
 </div>
--- src/lib/forms/TextInput.svelte
+++ src/lib/forms/TextInput.svelte
@@ -1,21 +1,59 @@
 <script lang="ts">
+  import { slide } from "svelte/transition";
+
   export let isNecessary: boolean;
   export let label: string;
   export let inputVal: string;
   export let placeholder: string;
   export let alt: string;
-  export let desc: string;
+  export let desc: string[];
+  export let validateInput: any;
+
+  let errorMessage: string[];
+
+  export let isValid: boolean;
+
+  $: errorMessage = validateInput(inputVal);
+  $: console.log(errorMessage);
 </script>
 
-<div class="flex">
-  <label class="font-bold mr-10">
+<div class="flex flex-col sm:flex-row my-3">
+  <label class="font-bold mr-10" for={label}>
     {#if isNecessary}
       <span class="text-red-500">*</span>
     {/if}
     {label}
   </label>
   <div class="flex flex-col w-[200px]">
-    <input type="text" class="" {alt} {placeholder} bind:value={inputVal} />
-    <p class="text-slate-600 text-sm">{desc}</p>
+    <input
+      name={alt}
+      type="text"
+      class="shadow-md 
+        rounded-sm 
+        transitions-all 
+        ease-in 
+        border-2 
+        border-black 
+        focus:outline-sky-500 
+        focus:outline-2 
+        bg-white 
+        pl-2
+        py-1
+        my-2
+        "
+      {alt}
+      {placeholder}
+      bind:value={inputVal}
+    />
+    {#if !isValid}
+      {#each errorMessage as error}
+        <p transition:slide class="text-red-600">{error}</p>
+      {/each}
+    {/if}
+    {#each desc as line}
+      <p class="text-slate-600 text-sm my-1">
+        {line}
+      </p>
+    {/each}
   </div>
 </div>
