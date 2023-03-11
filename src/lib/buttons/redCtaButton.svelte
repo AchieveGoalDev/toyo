491549bc7eee28f2a9dc3c490df581db6356c6c5
Jasumasa <thejasumasa@gmail.com>
2023-03-11 22:22:42 +0900

    update buttons to margin auto

--- src/lib/buttons/redCtaButton.svelte
+++ src/lib/buttons/redCtaButton.svelte
@@ -5,7 +5,7 @@
 
 <a
   {href}
-  class={"block text-3xl font-bold text-white p-5 bg-rose-700 rounded-full shadow-lg transition ease-in hover:bg-rose-600 hover:-translate-y-2"}
+  class={"block text-3xl font-bold text-white p-5 bg-rose-700 rounded-full shadow-lg transition ease-in hover:bg-rose-600 hover:-translate-y-2 mx-auto"}
 >
   {text}
 </a>
