53332ff333c60174ada813bdf10b2c7fa736b22c
Jasumasa <thejasumasa@gmail.com>
2023-08-06 16:32:32 +0900

    Create provisional article card and article object

---
+++ src/lib/news/ArticleCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import Tag from "./Tag.svelte";
+    import type { Article } from "./ArticleDef";
+
+    export let articleData: Article;
+</script>
+
+<div class="shadow-md bg-white">
+    <h2>
+        {articleData.title}
+    </h2>
+    <p>{articleData.creationDate.toLocaleDateString("ja-JP")}</p>
+    <div class="flex flex-row">
+        {#each articleData.tags as tag}
+            <Tag {tag} toUpdate={false} />
+        {/each}
+    </div>
+    <img src={articleData.imageUrl} alt="" />
+</div>
--- src/lib/news/TagDefs.ts
+++ src/lib/news/TagDefs.ts
@@ -4,22 +4,22 @@ export class Tag {
     color: string;
     isSelected: boolean;
 
-    constructor(text: string, color: string, id: number){
+    constructor(text: string, color: string, id: number) {
         this.id = id
         this.text = text
         this.color = color
         this.isSelected = false
     }
 
-    setText(text: string){
+    setText(text: string) {
         this.text = text;
     }
 
-    setColor(color: string){
+    setColor(color: string) {
         this.color = color;
     }
 
-    toggleIsSelected(): void{
+    toggleIsSelected(): void {
         this.isSelected = !this.isSelected;
     }
 }
@@ -31,7 +31,7 @@ export const newsTags: TagArray = [
     new Tag("お申込みについて", "bg-sky-700", 1),
     new Tag("イベントのお知らせ", "bg-teal-700", 2),
     new Tag("授業について", "bg-red-700", 3),
-    new Tag("休講について", "bg-purple-700",4),
+    new Tag("休講について", "bg-purple-700", 4),
     new Tag("次学期の募集について", "bg-green-700", 5),
     new Tag("その他", "bg-black", 6),
 ]
