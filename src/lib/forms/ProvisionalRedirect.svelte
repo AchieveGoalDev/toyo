7c63dca23c3da878d66b4b2675a8419b240f180a
Justin <thejasumasa@gmail.com>
2023-09-25 17:55:16 +0900

    cant rightly recall, maybe something important

---
+++ src/lib/forms/ProvisionalRedirect.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import ImageSelect from "./input/ImageSelect.svelte";
+  import { fly } from "svelte/transition";
+
+  import { ImageSelectInput } from "./data/typeDefs";
+
+  import { campusImageSelectParams } from "./data/basicDefs";
+
+  let select = new ImageSelectInput(campusImageSelectParams);
+
+  let redirect = "http://old.achieve-english.jp/entry/cat/";
+  let selected = "未選択";
+
+  $: selected = select.value.toLowerCase();
+</script>
+
+<div>
+  キャンパスを選択してください
+  <ImageSelect bind:data={select} />
+  {#if selected !== "未選択"}
+    <a transition:fly href={redirect + selected}>選択</a>
+  {/if}
+</div>
